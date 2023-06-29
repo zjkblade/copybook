2794cb56bbee9c7fad048a3af817f5f76e536875
zjk <1620662089@qq.com>
2023-06-29 17:59:36 +0800

    1

---
+++ test.sh
@@ -0,0 +1,15 @@
+# 新分支
+git checkout --orphan branch_name
+# 添加文件
+git add -A
+# 提交commit
+git commit -am "1"
+# 删除原来分支
+git branch -D master
+# 重命名分支
+git branch -m master
+# 提交到远程
+git push -f origin master
+
+
+#ncu
